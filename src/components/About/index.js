36161aaae6c29b1564fef0863a7272033b74ed6d
rrich-kray <rrich.kray@gmail.com>
2022-04-28 11:56:13 -0400

    worked on bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,32 +2,30 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about-container d-flex w-100 justify-content-center align-items-center">
-      <div className="left-panel justify-content-center align-items-center d-flex">
+    <div className="about-container d-flex flex-column w-100 justify-content-center align-items-center">
+      <div className="left-panel panel justify-content-center align-items-center d-flex">
         <div className="pfp-container">
           <img src={require("./avatar2.jpg")} className="pfp" alt="pfp"></img>
         </div>
       </div>
-      <div className="right-panel d-flex justify-content-center align-items-center">
+      <div className="right-panel panel d-flex justify-content-center align-items-center">
         <div className="bio-container">
-          <h1 data-testid="h1test" className="about-text-container">
+          <h1 data-testid="h1test" className="about-title">
             About Me
           </h1>
-          <p data-testid="paragraph-test">
-            Veniam ex reprehenderit consequat ex quis ad quis aliquip. Voluptate
-            sunt esse et eiusmod ut ullamco amet. Laboris anim ullamco cupidatat
-            laborum do consequat tempor sint occaecat ad. Pariatur ut qui esse
-            minim quis dolore enim id tempor deserunt. Sint ullamco occaecat
-            cillum magna nisi. Ipsum quis irure enim incididunt proident dolor
-            ad ad enim esse incididunt esse. Lorem velit laboris nostrud dolor
-            labore eu irure reprehenderit labore. Reprehenderit ad velit labore
-            eu veniam voluptate amet tempor est Lorem aliquip ea excepteur.
-            Ipsum irure velit excepteur duis elit dolor occaecat culpa dolore
-            excepteur. Aliqua excepteur sunt voluptate veniam consectetur. Magna
-            enim ad et sit do sit dolore esse ea. Eu ut excepteur tempor eiusmod
-            in. Exercitation elit do esse dolor. Velit adipisicing est ex ad
-            esse deserunt officia proident adipisicing magna incididunt.
-            Incididunt nisi non veniam deserunt qui laboris eu quis.
+          <p data-testid="paragraph-test" className="about-text-container">
+            My name is Ryan, and I am currently a student attending the Case
+            Western Reserve Full Stack Web Development Bootcamp. My career began
+            in equity research, where I produced research for institional
+            investor clients such as pension funds. Here, I frequently used
+            Python to build and organize large proprietary datasets that often
+            served as the bedrock for differentiated, revenue-generating
+            research. More importantly, it helped me realize my passion for
+            software development and motivated my departure from the industry
+            nearly a year ago. Since then, I have built dozens of projects
+            leveraging technologies across the entire MERN stack, and I continue
+            to be motivated by the prospect of developing unique solutions to
+            unique problems.
           </p>
         </div>
       </div>
