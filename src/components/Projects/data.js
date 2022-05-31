1a524703850ba9717641f3217707ad0bbafa8b3c
rrich-kray <rrich.kray@gmail.com>
2022-05-31 12:12:17 -0400

    added React landing page

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -151,4 +151,13 @@ export default [
     backgroundImage:
       "https://raw.githubusercontent.com/rrich-kray/github-user-search/main/src/assets/screen1.png",
   },
+  {
+    id: 17,
+    name: "React Landing Page",
+    ghRepository: "https://github.com/rrich-kray/simple-react-site",
+    deploymentLink: "https://simple-react-site-rrich-kray.herokuapp.com/",
+    description: "Landing page built with React",
+    backgroundImage:
+      "https://github.com/rrich-kray/simple-react-site/blob/main/src/assets/images/screen.png?raw=true",
+  },
 ];
